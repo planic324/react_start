cdbf704fd74a6b39e90c36520b76db3b177ae986
RohEunChan <nong0324@naver.com>
2023-01-20 17:39:14 +0900

    [생활코딩] 	react 17 ~ 20

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -6,12 +6,26 @@ import Navlist from "./components/Navlist"
 import Document from "./components/Document"
 
 class App extends Component{
+  // 컴포넌트를 초기화 시키는 함수
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'World Wide Web!'},
+      contents: [
+        {id:1, title:'html', desc:'HTML is Hyper...'},
+        {id:2, title:'css', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'Javascript is for interactive'}
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <Subject title="web" sub="world wide web!"></Subject>
-        <Subject title="React" sub="For UI"></Subject>
-        <Navlist></Navlist>
+        <Subject 
+        title="{this.state.subject.title}" 
+        sub="{this.state.subject.sub}"></Subject>
+        <Navlist data={this.state.contents}></Navlist>
         <Document title="HTML" sub="HTML is HyperText Markup Language."></Document>
       </div>
     );
--- react-app/src/components/Navlist.js
+++ react-app/src/components/Navlist.js
@@ -1,13 +1,21 @@
 import React, { Component} from 'react';
 
 class Navlist extends Component {
+
+  
     render () {
+      let lists = [];
+      let data = this.props.data;
+      let i = 0;
+
+      while (i < data.langth) {
+        lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
+        i = i + 1;
+      }
       return(
         <nav>
               <ul>
-                  <li><a href="1.html">HTML</a></li>
-                  <li><a href="2.html">CSS</a></li>
-                  <li><a href="3.html">JavaScript</a></li>
+                  {lists}
               </ul>
           </nav>
       );
