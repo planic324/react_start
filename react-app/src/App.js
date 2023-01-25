f225942cf023c318879cf617cb0ef2ff56796513
RohEunChan <nong0324@naver.com>
2023-01-25 17:53:23 +0900

    [생활코딩] 	react 21 ~ 22

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -10,11 +10,13 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state = {
+      mode: 'welcome',
       subject:{title:'WEB', sub:'World Wide Web!'},
+      welcome :{title: 'welcome', desc: 'hellow. react!!'},
       contents: [
         {id:1, title:'html', desc:'HTML is Hyper...'},
         {id:2, title:'css', desc:'CSS is for design'},
-        {id:3, title:'JavaScript', desc:'Javascript is for interactive'}
+        {id:3, title:'JavaScript', desc:'Javascript is for interactive'},
       ]
     }
   }
@@ -25,8 +27,7 @@ class App extends Component{
         <Subject 
         title={this.state.subject.title}
         sub={this.state.subject.sub}></Subject>
-        <Navlist 
-        data={this.state.contents}></Navlist>
+        <Navlist data={this.state.contents}></Navlist>
         <Document title="HTML" sub="HTML is HyperText Markup Language."></Document>
       </div>
     );
--- react-app/src/components/Navlist.js
+++ react-app/src/components/Navlist.js
@@ -2,20 +2,20 @@ import React, { Component} from 'react';
 
 class Navlist extends Component {
     render () {
-      const lists = [];
+      let lists = [];
       let data = this.props.data;
       let i = 0;
 
-      while (i < data.langth) {
-        lists.push(<li><a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
+      while (i < data.length) {
+        lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
         i = i + 1;
       }
       return(
         <nav>
-              <ul>
-                  {lists}
-              </ul>
-          </nav>
+            <ul>
+                {lists}
+            </ul>
+        </nav>
       );
     }
   }
